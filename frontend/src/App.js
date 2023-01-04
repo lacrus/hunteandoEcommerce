8231ef1b7643d3879418210ce4542f6a4d7c00b7
JC <juancruzmusso@gmail.com>
2023-01-04 12:50:32 +0100

    iniciando npx

---
+++ frontend/src/App.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./App.css";
+
+// COMPONENTES
+import AppBar from "./components/AppBar/AppBar";
+import HomePage from "./components/Home/Home";
+
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Cart from "./components/Cart/Cart.jsx";
+// import { Products } from "./containers/Products";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
+import Footer from "./components/Footer/Footer";
+import BotonWapp from "./components/BotonWapp/BotonWapp";
+import DashboardUsuario from "./components/Dashboard/DashboardUsuario/DashboardUsuario";
+import DashboardAdmin from "./components/Dashboard/DashboardAdmin/DashboardAdmin";
+
+function App() {
+  const usuario = useSelector((e) => e.usuario);
+
+  useEffect(() => {
+    if (!usuario.username) {
+      localStorage.removeItem("token");
+    }
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <AppBar usuario={usuario} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/:id" element={<Login />} />
+        <Route path="/registrarse" element={<Register />} />
+        <Route path="/carrito" element={<Cart />} />
+        {/* <Route path="/productos" element={<Products />} />
+        <Route path="/productos/detalle/:id" element={<ProductDetail />} /> */}
+        <Route
+          path="/dashboard/admin"
+          element={
+            // usuario.idAdmins === true ? (
+            <DashboardAdmin usuario={usuario} />
+            // ) : (
+            //   <Navigate to="/" replace={true} />
+            // )
+          }
+        />
+        <Route
+          path="/dashboard/client"
+          element={
+            // usuario.username ? (
+            <DashboardUsuario usuario={usuario} />
+            // ) : (
+            //   <Navigate to="/" replace={true} />
+            // )
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+      </Routes>
+      <Footer />
+      <BotonWapp />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Provider } from "react-redux";
+import store from "./redux/store/index";
+import axios from "axios";
+import App from './App';
+
+// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = "http://localhost:3001";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
