ecf0c46aeff2158946ca372c8d826943f6ea261b
JC <juancruzmusso@gmail.com>
2023-01-04 22:52:42 +0100

    variable entorno front

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,8 @@ import store from "./redux/store/index";
 import axios from "axios";
 import App from './App';
 
-// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+// axios.defaults.baseURL = "http://localhost:3001";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
