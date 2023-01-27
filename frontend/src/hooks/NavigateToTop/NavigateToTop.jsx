626c102d008a197f3c313172310a5ba1fdc9dced
JC <juancruzmusso@gmail.com>
2023-01-27 16:42:16 +0100

    vuelve pathname

--- frontend/src/hooks/NavigateToTop/NavigateToTop.jsx
+++ frontend/src/hooks/NavigateToTop/NavigateToTop.jsx
@@ -4,11 +4,11 @@ import { useLocation } from "react-router-dom";
 export default function NavigateToTop() {
   const { pathname } = useLocation();
 
-  // console.log(pathname)
+  console.log(pathname)
 
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, [pathname]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return null;
 }
