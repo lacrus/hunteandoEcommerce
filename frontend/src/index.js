1e9e91aa6e4613e7a77b8401a069730a267e0ec8
JC <juancruzmusso@gmail.com>
2023-01-29 22:40:09 +0100

    baseURL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,8 @@ import store from "./redux/store/index";
 import axios from "axios";
 import App from "./App";
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:3001";
 // axios.defaults.baseURL = "http://localhost:3001";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
